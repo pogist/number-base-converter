6015672b08d4f9212c0fb7c124d406d4916d955a
pogist <murilo.paixao.2@gmail.com>
2018-03-19 12:48:37 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject number-base-converter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A simple number base converter program written in Clojure. Built for a college project."
+  :url "https://github.com/pogist/number-base-converter"
+  :license {:name "The MIT License (MIT)"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot number-base-converter.core
   :target-path "target/%s"
